ae429945bdbfd90ba32848aaa9edbd0651f302d8
BruceLeeVanDuc <DucLV.B22CN228@stu.ptit.edu.vn>
2026-07-12 00:22:11 +0700

    khoi tao

---
+++ product-service/src/main/java/microservice_product_service/controller/ProductController.java
@@ -0,0 +1,20 @@
+package microservice_product_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @GetMapping
+    public String getProducts() {
+        return "Danh sách sản phẩm từ Product Service";
+    }
+    
+}
